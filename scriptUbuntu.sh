1138081b83d69a3779bbd4fbd32863f1189e9680
Luan Rodrigues da Silva <112918012+minacabu@users.noreply.github.com>
2024-04-23 05:59:25 +0000

    Atualizações

--- scriptUbuntu.sh
+++ scriptUbuntu.sh
@@ -17,131 +17,85 @@ sudo rm /var/cache/apt/archives/lock
 
 ## Atualizando o repositório e baixando o git ##
 sudo apt update -y
-sudo apt install git
+sudo apt install git nala -y
 
-## Driver de video ##
-PPA_GRAPHICS_DRIVERS="ppa:graphics-drivers/ppa"
-sudo apt-add-repository "$PPA_GRAPHICS_DRIVERS" -y
-sudo add-apt-repository ppa:kisak/kisak-mesa && sudo dpkg --add-architecture i386 && sudo apt update && sudo apt upgrade && sudo apt install libgl1-mesa-dri:i386 mesa-vulkan-drivers mesa-vulkan-drivers:i386
+## Adicionando repositórios adicionais ##
+sudo add-apt-repository ppa:graphics-drivers/ppa -y
+sudo add-apt-repository ppa:kisak/kisak-mesa -y
+sudo dpkg --add-architecture i386 
+sudo apt update 
+sudo apt upgrade -y 
+sudo apt install libgl1-mesa-dri:i386 mesa-vulkan-drivers mesa-vulkan-drivers:i386 -y
 
+## Instalando programas via Flatpak ##
+flatpak_packages=(
+    "com.jetbrains.IntelliJ-IDEA-Ultimate"
+    "com.jetbrains.PyCharm-Professional"
+    "com.jetbrains.DataGrip"
+    "com.visualstudio.code"
+    "com.getpostman.Postman"
+    "com.microsoft.Edge"
+    "io.gitlab.librewolf-community"
+    "com.github.micahflee.torbrowser-launcher"
+    "io.gitlab.news_flash.NewsFlash"
+    "com.bitwarden.desktop"
+    "in.srev.guiscrcpy"
+    "org.qbittorrent.qBittorrent"
+    "org.onlyoffice.desktopeditors"
+    "org.filezilla_project.Filezilla"
+    "com.obsproject.Studio"
+    "org.videolan.VLC"
+    "org.gimp.GIMP"
+    "org.upscayl.Upscayl"
+    "com.discordapp.Discord"
+    "io.github.jeffshee.Hidamari"
+    "com.mattjakeman.ExtensionManager"
+    "com.stremio.Stremio"
+    "com.dropbox.Client"
+    "net.cozic.joplin_desktop"
+    "com.protonvpn.www"
+    "com.github.IsmaelMartinez.teams_for_linux"
+)
 
-## Flatpack e os programas  ##
-sudo apt update -y
-sudo apt install flatpak -y
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+for package in "${flatpak_packages[@]}"; do
+    flatpak install flathub "$package" -y
+done
 
-##programação##
-flatpak install flathub com.jetbrains.IntelliJ-IDEA-Ultimate -y
-flatpak install flathub com.jetbrains.PyCharm-Professional -y
-flatpak install flathub com.jetbrains.DataGrip -y
-flatpak install flathub com.visualstudio.code -y
-flatpak install flathub com.getpostman.Postman -y
+## Instalando VirtualBox e outros programas via APT ##
+sudo apt install virtualbox scrcpy flameshot gnome-tweaks net-tools -y
 
-##navegador##
-flatpak install flathub com.microsoft.Edge -y
-flatpak install flathub io.gitlab.librewolf-community -y
-flatpak install flathub com.github.micahflee.torbrowser-launcher -y
+## Instalando Droidcam ##
+wget -O /tmp/droidcam_client_amd64.deb https://beta.droidcam.app/go/droidCam.client.setup.deb
+sudo dpkg -i /tmp/droidcam_client_amd64.deb
+sudo apt install -f -y
+sudo apt install linux-headers-$(uname -r) v4l2loopback-dkms -y
 
-##outros##
-flatpak install flathub io.gitlab.news_flash.NewsFlash -y
-flatpak install flathub com.authy.Authy -y
-flatpak install flathub com.bitwarden.desktop -y
-flatpak install flathub org.telegram.desktop -y
-flatpak install flathub in.srev.guiscrcpy -y
-flatpak install flathub org.qbittorrent.qBittorrent -y
-
-##Office##
-flatpak install flathub org.onlyoffice.desktopeditors -y
-
-##FTP##
-flatpak install flathub org.filezilla_project.Filezilla -y
-
-##OBS##
-flatpak install flathub com.obsproject.Studio -y
-
-##VLC##
-flatpak install flathub org.videolan.VLC -y
-
-
-##Imagem##
-flatpak install flathub org.gimp.GIMP -y
-flatpak install flathub org.upscayl.Upscayl -y
-
-##virtualização de linux##
-sudo apt install virtualbox -y
-
-##Discord##
-flatpak install flathub com.discordapp.Discord -y
-
-##papel de parede animado##
-flatpak install flathub io.github.jeffshee.Hidamari -y
-
-##gerenciador de extensões##
-flatpak install flathub com.mattjakeman.ExtensionManager -y
-
-##print massa#
-flatpak install flathub org.flameshot.Flameshot -y
-
-##produtividade##
-flatpak install flathub md.obsidian.Obsidian -y
-
-##Jogos#
-flatpak install flathub net.lutris.Lutris -y
-flatpak install flathub com.valvesoftware.Steam -y
-flatpak install flathub com.heroicgameslauncher.hgl -y
-
-##VPN##
-flatpak install flathub com.protonvpn.www -y
-
-##Teams##
-flatpak install flathub com.github.IsmaelMartinez.teams_for_linux -y
-
-##Nuvem##
-wget https://mega.nz/linux/repo/xUbuntu_22.04/amd64/megasync-xUbuntu_22.04_amd64.deb && sudo apt install "$PWD/megasync-xUbuntu_22.04_amd64.deb" -y
-
-##Firewall##
-wget https://updates.safing.io/latest/linux_amd64/packages/portmaster-installer.deb && sudo apt installl "$PWD/portmaster-installer.deb" -y
-
-
-##gerenciador de temas##
-sudo apt install gnome-tweaks -y
-
-##ferramentas de redes##
-sudo apt install net-tools -y
-
-sudo apt install flameshot -y
-
-sudo apt install docker-ce -y
-
-
-## Frescura do sistema##
-echo 'Configurando GNOME'
+## Configurações adicionais do GNOME ##
 gsettings set org.gnome.desktop.peripherals.touchpad disable-while-typing true
 gsettings set org.gnome.desktop.peripherals.touchpad click-method 'fingers'
 gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click  true
 gsettings set org.gnome.desktop.peripherals.touchpad two-finger-scrolling-enabled true
 gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll true
 
-
-## Finalização, atualização e limpeza##
+## Atualização e limpeza ##
 sudo apt update 
 sudo apt dist-upgrade -y 
 sudo apt upgrade -y 
 sudo snap refresh 
 sudo flatpak upgrade -y 
-flatpak uninstall --unused 
+flatpak uninstall --unused -y 
 sudo apt autoremove -y 
-sudo apt autoclean
+sudo apt autoclean -y
 
-sudo apt install gnome-sushi -y
+## Instalando mais programas ##
+sudo apt install gnome-sushi neofetch -y
 
-## mensagem gay##
-sudo apt install neofetch -y
-
+## Exibindo informações após a instalação ##
 neofetch
 
 echo
 echo
 echo "Instalação finalizada, é recomendado reiniciar o sistema."
 echo "thx! :P"
-echo 
+echo
+
