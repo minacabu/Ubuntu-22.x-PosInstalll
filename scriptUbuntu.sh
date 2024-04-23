ede138a7da66482609c2ab38b1f77ef4471f4601
Luan Rodrigues da Silva <112918012+minacabu@users.noreply.github.com>
2024-04-23 06:08:01 +0000

    Atualização 2

--- scriptUbuntu.sh
+++ scriptUbuntu.sh
@@ -64,6 +64,27 @@ done
 ## Instalando VirtualBox e outros programas via APT ##
 sudo apt install virtualbox scrcpy flameshot gnome-tweaks net-tools -y
 
+##Docker##
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+##ffish
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt update
+sudo apt install fish
+
 ## Instalando Droidcam ##
 wget -O /tmp/droidcam_client_amd64.deb https://beta.droidcam.app/go/droidCam.client.setup.deb
 sudo dpkg -i /tmp/droidcam_client_amd64.deb
